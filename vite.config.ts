d8c858d4d0a769514e589aabe63480aa5e622eba
Anurag kumar <Anuragkr8651@gmail.com>
2025-02-16 01:27:44 +0530

    fixed refresh problem

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,24 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  base: "/",
 })
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     historyApiFallback: true
+//   }
+// });
